bf08f44e64aac6ec5ed72dba139ceaebd5b3021c
Perberto07 <164364781+Perberto07@users.noreply.github.com>
2025-06-14 00:56:41 +0800

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8000/api/token/', {
+      const response = await axios.post('https://grocery-delta-six.vercel.app/api/token/', {
         username,
         password,
       });
