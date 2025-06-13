872717aa5034d2e35d1c9ec1a5c9aea12f8d7b2f
Perberto07 <164364781+Perberto07@users.noreply.github.com>
2025-06-13 22:23:30 +0800

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,9 @@ import MainLayout from '../layouts/MainLayout';
 const Home = () => {
   return (
     <MainLayout>
-      <h1>Welcome to our website</h1>
+      <div className='flex justify-center items-center mt-28'>
+        <h1 className='text-3xl font-semibold uppercase text-center'>Welcome to Liza's Mini Grocery</h1>
+      </div>
     </MainLayout>
   );
 };
