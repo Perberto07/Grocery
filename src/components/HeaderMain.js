0c6fb1a186668eb30c2fd3bf6f64a7b229cbce92
Perberto07 <164364781+Perberto07@users.noreply.github.com>
2025-06-17 21:27:38 +0800

    a

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -36,7 +36,7 @@ const HeaderMain = () => {
             </li>
             <li className="flex items-center gap-2 hover:text-blue-400">
               <LogIn size={16} />
-              <Link to="/">Dashboard</Link>
+              <Link to="/dashboard">Dashboard</Link>
             </li>
           </ul>
         </nav>
@@ -60,7 +60,7 @@ const HeaderMain = () => {
             </li>
             <li className="flex items-center gap-2 hover:text-blue-400">
               <LogIn size={16} />
-              <Link to="/">Dashboard</Link>
+              <Link to="/dashboard">Dashboard</Link>
             </li>
           </ul>
         </nav>
