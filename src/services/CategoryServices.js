f19f90ee371bf26950ef4e87e8ea012832019693
Perberto07 <164364781+Perberto07@users.noreply.github.com>
2025-06-27 20:51:23 +0800

    ala

--- src/services/CategoryServices.js
+++ src/services/CategoryServices.js
@@ -2,22 +2,31 @@ import axios from "axios";
 
 const API_URL = 'https://backendgrocery-5rpu.onrender.com/category/';
 
-export const getCategory = async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
-}
-
-export const createCategory = async (categoryData) => {
-    const response = await axios.post(API_URL, categoryData);
-    return response.data;
-}
-
-export const updateCategory = async (id, categoryData) => {
-    const response = await axios.put(`${API_URL}${id}/`, categoryData);
-    return response.data;
+const getAuthHeaders = () => {
+  const token = localStorage.getItem('access_token');
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 };
 
-export const deleteCategory = async(id) =>{
-    const response = await axios.delete(`${API_URL}${id}/`);
-    return response.data;
+export const getCategory = async () => {
+  const response = await axios.get(API_URL, getAuthHeaders());
+  return response.data;
+};
+
+export const createCategory = async (categoryData) => {
+  const response = await axios.post(API_URL, categoryData, getAuthHeaders());
+  return response.data;
+};
+
+export const updateCategory = async (id, categoryData) => {
+  const response = await axios.put(`${API_URL}${id}/`, categoryData, getAuthHeaders());
+  return response.data;
+};
+
+export const deleteCategory = async (id) => {
+  const response = await axios.delete(`${API_URL}${id}/`, getAuthHeaders());
+  return response.data;
 };
